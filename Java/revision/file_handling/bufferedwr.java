8a85d494eff07c1674912d427f673b98b6a1322f
sreelakshmiV7 <sreelakshmiv009@gmail.com>
2025-11-23 23:28:59 +0530

    solving the issue of overwritting

--- Java/revision/file_handling/bufferedwr.java
+++ Java/revision/file_handling/bufferedwr.java
@@ -5,10 +5,11 @@ class bufferedwr{
     public static void main(String []args)throws IOException
     {
         Scanner sc=new Scanner(System.in);
-        BufferedWriter bf=new  BufferedWriter(new FileWriter("text.txt"));
+        BufferedWriter bf=new  BufferedWriter(new FileWriter("text.txt", true));
         System.out.println("enter the text:");
         String s=sc.nextLine();
-        bf.write(s);
+        bf.write(s+"\n");
+        bf.flush();
         bf.close();
 
         BufferedReader br=new BufferedReader(new FileReader("text.txt"));
